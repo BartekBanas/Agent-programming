6d999ec0db84c0e98535ca652380410d1c4bf662
Bartłomiej Banaś <bartek.banas675@gmail.com>
2023-10-17 13:22:33 +0200

    Add task 1 of lab01

---
+++ lab01/src/NumberConverter.java
@@ -0,0 +1,25 @@
+// lab01, task 1
+
+public class NumberConverter {
+    public static void main(String[] args) {
+        printConvertedNumbers(0, 100);
+    }
+
+    public static void printConvertedNumbers(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            System.out.println(convertNumber(i));
+        }
+    }
+
+    public static String convertNumber(int number) {
+        if (number % 3 == 0 && number % 5 == 0) {
+            return "xyxy";
+        } else if (number % 3 == 0) {
+            return "xxx";
+        } else if (number % 5 == 0) {
+            return "yyy";
+        } else {
+            return Integer.toString(number);
+        }
+    }
+}
