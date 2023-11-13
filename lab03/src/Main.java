8e98a2220e83a3d9dee4ee1c5a856f2c056c543c
Bartłomiej Banaś <bartek.banas675@gmail.com>
2023-11-13 14:31:57 +0100

    Rename default agent name

--- lab03/src/Main.java
+++ lab03/src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
         try {
             // Agent 1
-            AgentController agentController = container.createNewAgent("First Agent", "AgentLooper", null);
+            AgentController agentController = container.createNewAgent("Agent", "AgentLooper", null);
 
             agentController.start();
             Thread.sleep(5000);
